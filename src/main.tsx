9c35806700de6a9cc48d59ba8de9706c25ef3105
Rukaiyabvm <ayishagani18@gmail.com>
2025-11-19 22:11:02 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <App />
 );
+
 
